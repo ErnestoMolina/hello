50ec6bb002a0dacd3ca813f20101ec6d575c8dbb
ErnestoMolina <helverzon@gmail.com>
2024-06-06 23:21:32 -0500

    manejar un error

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ErnestoMolina/greetings"
 )
 
 func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
 	fmt.Println()
-	message := greetings.Hello("Helverzon")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
